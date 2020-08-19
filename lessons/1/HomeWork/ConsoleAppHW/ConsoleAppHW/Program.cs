33c12f656245eb70ee2beef7ce173cb31e7ed690
SbnUfo <sibirevao@yandex.ru>
2020-08-19 14:56:59 +0300

    HW 1l add

--- lessons/1/HomeWork/ConsoleAppHW/ConsoleAppHW/Program.cs
+++ lessons/1/HomeWork/ConsoleAppHW/ConsoleAppHW/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Threading;
 
-namespace ConsoleAppHW
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name;
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
+            Thread.Sleep(5000);
+            Console.WriteLine("Hello, " + name);
+            Thread.Sleep(5000);
+            Console.WriteLine("Bye, " + name);
+            Console.ReadLine();
         }
     }
 }
+
