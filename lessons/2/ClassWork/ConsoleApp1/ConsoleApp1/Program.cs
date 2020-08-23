909ec3a41cb900ce35956fbd14f46d337f7bef92
SbnUfo <sibirevao@yandex.ru>
2020-08-23 14:23:03 +0300

    CW2

---
+++ lessons/2/ClassWork/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string bb = "olga";
+            char aa = '2';
+
+            int f = 33;
+            System.Int32 gh;
+
+            
+        }
+    }
+}
