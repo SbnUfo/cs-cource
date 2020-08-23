ae1fdaa18267431f5200196a1e5cf5c0d0ef1d13
SbnUfo <sibirevao@yandex.ru>
2020-08-23 17:40:11 +0300

    Second HomeWork

---
+++ lessons/2/HomeWork/ConsoleAppHW2/ConsoleAppHW2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace ConsoleAppHW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Console.WriteLine("Enter two integer numbers: ");
+             double a = double.Parse(Console.ReadLine());
+             double b = double.Parse(Console.ReadLine());
+             Console.WriteLine("Sum: " + (a + b));
+             Console.WriteLine("Difference: " + (a - b));
+             Console.WriteLine("Product: " + (a * b));*/
+
+
+
+
+
+
+            Console.WriteLine("Enter two numbers: ");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+             
+
+            
+            Console.WriteLine("Specify the type of operation (+,-,*,/, %, ^): ");
+            char o = char.Parse(Console.ReadLine());
+            double r;
+
+            if (o == '+')
+            {
+                r = a + b;
+                Console.WriteLine("Result: " + r);
+            }
+
+            else if (o == '-')
+            {
+                r = a - b;
+                Console.WriteLine("Result: " + r);
+            }
+
+            else if (o == '*')
+            {
+                r = a * b;
+                Console.WriteLine("Result: " + r);
+            }
+
+            else if (o == '/')
+            {
+                r = a / b;
+                Console.WriteLine("Result: " + r);
+            }
+
+            else if (o == '%')
+            {
+                r = a % b;
+                Console.WriteLine("Result: " + r);
+            }
+
+           /* else if (o == '^')
+            {
+                r = a ^ b;
+                Console.WriteLine("Result: " + r);
+            }*/
+
+
+        
+
+
+
+        }
+    }
+}
