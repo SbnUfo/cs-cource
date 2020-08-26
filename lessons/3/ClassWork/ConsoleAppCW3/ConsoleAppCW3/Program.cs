8d04010f99ae76acebcd40649a405b560427f612
SbnUfo <sibirevao@yandex.ru>
2020-08-26 15:16:06 +0300

    lesson 3 HW and CW

---
+++ lessons/3/ClassWork/ConsoleAppCW3/ConsoleAppCW3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleAppCW3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* object a = 5;
+             a = (int)a + 5;
+             Console.WriteLine(a);
+            dynamic b = 4;
+            b = b + 5;
+            Console.WriteLine(b);*/
+
+            string[] values = new string[5];
+
+            for (int i = 0; i < values.Length; i++) 
+            {
+                values[i] = Console.ReadLine();
+            }
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                Console.WriteLine(values[i]);
+            }
+
+
+
+            
+
+
+
+        }
+    }
+}
---
+++ lessons/3/HomeWork/ConsoleAppHW3/ConsoleAppHW3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleAppHW3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter three names: ");
+            string[] names = new string[3];
+                names[0] = Console.ReadLine();
+                names[1] = Console.ReadLine();
+                names[2] = Console.ReadLine();
+
+            
+            Console.WriteLine("Enter age of these people: ");
+            string[] age = new string[3];
+            age[0] = Console.ReadLine();
+            age[1] = Console.ReadLine();
+            age[2] = Console.ReadLine();
+
+ 
+            for (int i = 0; i < names.Length; i++)
+            {
+                int a = int.Parse(age[i]) + 4;
+                Console.WriteLine("Name: " + (names[i]) + ", age in 4 years: "+ (a));
+            }
+
+
+        }
+    }
+    
+}
