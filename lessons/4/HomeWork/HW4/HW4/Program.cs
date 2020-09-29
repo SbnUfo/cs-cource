426d807249be086b588ef6f30204b1db15a58fa5
SbnUfo <sibirevao@yandex.ru>
2020-09-29 23:49:35 +0300

    HW4

--- lessons/4/HomeWork/HW4/HW4/Program.cs
+++ lessons/4/HomeWork/HW4/HW4/Program.cs
@@ -2,11 +2,48 @@
 
 namespace HW4
 {
+    enum Containers
+    {
+        OneL = 1,
+        FiveL = 5,
+        TwentyL = 20,
+    }
+
+
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Какой объем сока (в литрах) требуется упаковать?");
+            double volume = Double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+
+            Containers[] bottles = (Containers[])Enum.GetValues(typeof(Containers));
+            int[] NBottle = new int[bottles.Length];
+
+            int ContainersFlags = 0;
+            for (int i = bottles.Length - 1; i > 0; i--)
+            {
+                NBottle[i] = (int)volume / (int)bottles[i];
+                if (NBottle[i] > 0)
+                {
+                    ContainersFlags |= 1 << i;
+                    volume %= (int)bottles[i];
+                }
+            }
+
+            NBottle[0] = (int)Math.Round(volume / (int)bottles[0]);
+            if (NBottle[0] > 0)
+                ContainersFlags |= 1;
+
+            for (int i = bottles.Length - 1; i >= 0; i--)
+            {
+                if ((ContainersFlags & 1 << i) == 1 << i)
+
+                    Console.WriteLine("Вам потребуются следующие контейнеры: {0,4:d} л: {1}шт.", bottles[i], NBottle[i]);
+
+            }
+
         }
     }
 }
