97b908557de939b69dd855e4c246bab5fef5bb0b
SbnUfo <sibirevao@yandex.ru>
2020-10-02 19:51:34 +0300

    HW8

---
+++ lessons/14/ClassWork/CW14/CW14/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CW14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var a = new ErrorList("test1", new List<string> { "testerror1", "testerror2" }))
+                Console.WriteLine($"Category: {a.Category}, Errors: {a.Errors}");
+
+            foreach(var item in a)
+            {
+
+            }
+
+        }
+    }
+
+    class ErrorList : IDisposable, IEnumerable<string>
+    {
+        public string Category { get; }
+
+        public List<string> Errors { get; set; }
+
+        public ErrorList(string category, List<string> errors)
+        {
+            Category = category;
+            Errors = errors;
+        }
+        public void Add(string errorMessage)
+        {
+            Errors.Add($"{errorMessage} {Category}");
+        }
+        public IEnumerator<string> GetEnumerator()
+        {
+            return Errors.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return Errors.GetEnumerator();
+        }
+        public void Dispose()
+        {
+            if (Errors == null)
+            {
+                return;
+            }
+            Errors.Clear();
+            Errors = null;
+        }
+    }
+}
+
---
+++ lessons/8/HomeWork/HW8/HW8/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HW8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var entered = Console.ReadLine();
+
+            if (IsBool(entered)) Console.WriteLine("True");
+            else Console.WriteLine("False");
+
+        }
+
+        static bool IsBool(string test)
+        {
+            var dictionary = new Dictionary<char, char>
+            {
+                { '(',')' },
+                {'[',']' },
+                {'{','}' },
+                {'<','>' }
+            };
+            var stack = new Stack<char>();
+            foreach (char i in test)
+            {
+                if (dictionary.ContainsKey(i))
+
+                    stack.Push(i);
+
+                else if (dictionary.ContainsValue(i))
+
+                    stack.Pop();
+
+            }
+            return stack.Count == 0;
+        }
+    }
+}
