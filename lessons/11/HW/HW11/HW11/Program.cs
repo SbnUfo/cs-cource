89ea43913583208e6354721b6fed2656df85d6fb
SbnUfo <sibirevao@yandex.ru>
2020-10-09 00:24:40 +0300

    HW11

---
+++ lessons/11/HW/HW11/HW11/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using System;
+
+namespace Reminder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reminder1 = new ReminderItem(DateTimeOffset.Parse("10.10.2020 8:00"), "Будильник");
+            Console.WriteLine(reminder1.WriteProperties());
+
+            var reminder2 = new ReminderItem(DateTimeOffset.Parse("10.10.2020 8:10"), "Повторный будильник");
+            Console.WriteLine(reminder2.WriteProperties());
+
+
+        }
+    }
+}
---
+++ lessons/11/HW/HW11/HW11/ReminderItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Reminder
+{
+    class ReminderItem
+    {
+        public DateTimeOffset AlarmDate { get; set; }
+
+        public string AlarmMessage { get; set; }
+
+        public TimeSpan TimeToAlarm =>
+            DateTimeOffset.Now - AlarmDate;
+
+        public bool IsOutdated =>
+            TimeToAlarm >= TimeSpan.Zero;
+
+        public ReminderItem(DateTimeOffset alarmDate, string alarmMessage)
+        {
+            AlarmDate = alarmDate;
+            AlarmMessage = alarmMessage;
+        }
+
+        public string WriteProperties() =>
+            $"AlarmDate: {AlarmDate}\nAlarmMessage: {AlarmMessage}\nTimeToAlarm: {TimeToAlarm}\nIsOutdated: {IsOutdated}\n";
+
+    }
+}
