5967c1a039600c88120b4e99f17f0f1ef6ec7fda
SbnUfo <sibirevao@yandex.ru>
2020-11-24 23:30:26 +0300

    HW17

---
+++ lessons/17/HomeWork/HW17/HW17/FileWriterWithProgress.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace HW17
+{
+    public class FileWriterWithProgress
+    {
+        public event EventHandler<Performed> WritingPerformed;
+        public event EventHandler<Completed> WritingCompleted;
+
+        public void WriteBytes(string fileName, byte[] data, float percentageToFireEvent)
+        {
+            using (FileStream fs = File.OpenWrite(fileName))
+            {
+                for (int i = 0; i <= data.Length; i++)
+                {
+                    if (i != 0 && i % (GetNumber(data, percentageToFireEvent)) == 0)
+                    {
+                        fs.Write(data, i - (GetNumber(data, percentageToFireEvent)), (GetNumber(data, percentageToFireEvent)));
+                        WritingPerformed?.Invoke(this, new Performed($"{fileName}", data, i / 100));
+                    }
+                }
+                WritingCompleted?.Invoke(this, new Completed("Writing Complited!"));
+            }
+        }
+        public static int GetNumber(byte[] data, float percentageToFireEvent)
+        {
+            return (int)(data.Length * percentageToFireEvent / 1.0);
+        }
+    }
+}
---
+++ lessons/17/HomeWork/HW17/HW17/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace HW17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileWriter = new FileWriterWithProgress();
+            var write = new Result();
+            byte[] data = new byte[10000];
+            var random = new Random();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = (byte)random.Next(255);
+            }
+
+            fileWriter.WritingPerformed += write.WriteResultData;
+            fileWriter.WritingCompleted += write.WriteResultFinished;
+            fileWriter.WriteBytes($"log.txt", data, 0.1f);
+        }
+    }
+    public class Completed : EventArgs
+    {
+        public string Text { get; set; }
+        public Completed(string text)
+        {
+            Text = text;
+        }
+    }
+    public class Performed : EventArgs
+    {
+        public string FileName { get; set; }
+        public byte[] Data { get; set; }
+        public float PercentageToFireEvent { get; set; }
+        public Performed(string fileName, byte[] data, float percentageToFireEvent)
+        {
+            FileName = fileName;
+            Data = data;
+            PercentageToFireEvent = percentageToFireEvent;
+        }
+    }
+    public class Result
+    {
+        public void WriteResultData(object sender, Performed args)
+        {
+            Console.WriteLine($"{args.PercentageToFireEvent} % is done");
+        }
+        public void WriteResultFinished(object sender, Completed args)
+        {
+            Console.WriteLine(args.Text);
+        }
+    }
+}
