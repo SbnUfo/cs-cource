6287cd68769b87d1b7f2b02c4e07557246ef84cd
SbnUfo <sibirevao@yandex.ru>
2020-12-02 22:55:06 +0300

    HW27

--- lessons/27/HomeWork/SQLQueryHW27.sql
+++ lessons/27/HomeWork/SQLQueryHW27.sql
@@ -1,24 +1,33 @@
 ﻿CREATE TABLE [Status](
     [Id] INT IDENTITY(1, 1) NOT NULL,
     [Name] VARCHAR(20) NOT NULL,
-
     CONSTRAINT PK_Status_Id PRIMARY KEY([Id]),
     CONSTRAINT UQ_Status_Name UNIQUE ([Name])
 );
+INSERT INTO [Status]
+VALUES
+    ('Created'),
+    ('Ready'),
+    ('Failed');
+
 CREATE TABLE [Contact](
     [Id] INT IDENTITY(1, 1) NOT NULL,
     [ContactId] VARCHAR(32) NOT NULL,
-
     CONSTRAINT PK_Contact_Id PRIMARY KEY ([Id]),
     CONSTRAINT UQ_Contact_ContactId UNIQUE ([ContactId])
-);
+)
+INSERT INTO [Contact]
+VALUES
+    ('Id001'),
+    ('Id002'),
+    ('Id003');
+
 CREATE TABLE [Reminder](
-    [Id] UNIQUEIDENTIFIER NOT NULL DEFAULT newid(),
-	[DateTime] DATETIMEOFFSET NOT NULL,
+    [Id] UNIQUEIDENTIFIER NOT NULL,
     [StatusId] INT NOT NULL,
+    [DateTime] DATETIMEOFFSET NOT NULL,
     [Message] VARCHAR(512) NOT NULL,
     [ContactId] INT NOT NULL,
-
     CONSTRAINT PK_Reminder_Id PRIMARY KEY ([Id]),
     CONSTRAINT FK__StatusId_StatusId FOREIGN KEY ([StatusId])
         REFERENCES [Status]([Id])
@@ -28,21 +37,9 @@ CREATE TABLE [Reminder](
         REFERENCES [Contact]([Id])
             ON DELETE CASCADE
             ON UPDATE CASCADE
-);
-
-INSERT INTO [Status] VALUES
-    ('Created'),
-    ('Ready'),
-    ('Sent'),
-    ('Failed')
-INSERT INTO [Contact] VALUES
-    ('Id001'),
-    ('Id002'),
-    ('Id003'),
-    ('Id004')
-insert into [Reminder] VALUES
-    (default, '2020-12-02 21:41:00 +03:00', 1, 'Сделать домашку №1', 1),
-    (default, '2020-12-02 21:42:00 +03:00', 2, 'Сделать домашку №2', 3),
-    (default, '2020-12-02 21:43:00 +03:00', 3, 'Сделать домашку №3', 2),
-    (default, '2020-12-02 21:44:00 +03:00', 4, 'Сделать домашку №4', 4)
-
+)
+INSERT INTO [Reminder] ([Id], [StatusId], [DateTime], [Message], [ContactId])
+VALUES
+    (NEWID(), 1, SYSUTCDATETIME(), 'Сделать домашку №1', 1),
+    (NEWID(), 1, SYSUTCDATETIME(), 'Сделать домашку №2', 1),
+    (NEWID(), 3, SYSUTCDATETIME(), 'Сделать домашку №3', 2)
