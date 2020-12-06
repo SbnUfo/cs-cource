79efcf6d58dbc000d1a229a7bb7c4e5d80981e34
SbnUfo <sibirevao@yandex.ru>
2020-12-07 00:23:15 +0300

    HW29

---
+++ lessons/29/HomeWork/SQLQueryHW29.sql
@@ -0,0 +1,45 @@
+﻿WITH AllOrdersWithTotalSum([CustomerId], [Year], [Total Price]) AS 
+(SELECT
+	Order.CustomerId,
+	YEAR(Order.OrderDate),
+	SUM(Product.Price * OrderLine.[Count]) * (1 - ISNULL(O.Discount, 0)) AS 'Total Price'
+FROM [Order] AS Order
+	JOIN [OrderLine] AS OrderLine 
+	ON Order.Id = OrderLine.OrderId
+	JOIN [Product] AS Product 
+	ON OrderLine.ProductId = Product.Id
+GROUP BY Order.CustomerId, 
+	YEAR(Order.OrderDate),
+	Order.Discount),
+
+TotalYearSum([CustomerId], [Year], [YearSum]) AS
+(
+SELECT
+	[CustomerId],
+	[Year],
+	SUM([Total Price])
+FROM AllOrdersWithTotalSum
+GROUP BY [Year],
+	[CustomerId]
+),
+
+MaxTotalYearSum([Year], [YearSum]) AS
+(
+SELECT
+    [Year],
+    MAX(YearSum)
+FROM TotalYearSum
+GROUP BY [Year]
+)
+
+
+SELECT
+    MaxTotalYearSum.[Year],
+    TotalYearSum.CustomerId,
+    Customer.Name,
+    MaxTotalYearSum.YearSum
+FROM MaxTotalYearSum AS MaxTotalYearSum
+	JOIN TotalYearSum AS TotalYearSum 
+	ON MaxTotalYearSum.YearSum = TotalYearSum.YearSum
+	JOIN [Customer] AS Customer 
+	ON TotalYearSum.CustomerId = Customer.Id
